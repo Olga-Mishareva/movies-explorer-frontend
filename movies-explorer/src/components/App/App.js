740691aa4451b28b540b07bd9b8197773548074c
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-07-29 20:03:53 +0200

    feat: add react router

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -1,9 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Header from '../Header/Header';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="page">
+      <Header />
+      <Routes>
+        <Route exact path="/"/>
+        <Route path="/movies"/>
+        <Route path="/saved-movies"/>
+        <Route path="/profile"/>
+        <Route path="/signup"/>
+        <Route path="/signin"/>
       
+
+      </Routes>
     </div>
   );
 }
