cf9761387192613cf9b46e1173a44a9bc6201fbb
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-07-31 13:32:33 +0200

    feat: add menu for not logged user

--- movies-explorer/src/components/Navigation/Navigation.js
+++ movies-explorer/src/components/Navigation/Navigation.js
@@ -7,15 +7,15 @@ function Navigation() {
     <div className="navigation">
       {/* invisible добавл. состоянием, если залогинен */}
       <nav className="navigation navigation_type_logged-out navigation_ "> 
-        <Link to="" className="navigation__link navigation__link_type_register"></Link>
-        <Link to="" className="navigation__link navigation__link_type_login"></Link>
+        <Link to="" className="navigation__link navigation__link_type_register">Регистрация</Link>
+        <Link to="" className="navigation__link navigation__link_type_login">Войти</Link>
       </nav>
       {/* background-image меняется состоянием при нажатии, invisible добавл. если не залог. */}
       <button className="navigation__button navigation__button_invisible " type="button"></button> 
       {/* invisible добавляется состоянием при нажатии на navigation__button, меню скрывается */}
       <div className="overlay overlay_invisible "> 
         {/* invisible добавл. состоянием, если не залогинен */}
-        <nav className="navigation navigation_type_logged-in navigation_ "> 
+        <nav className="navigation navigation_type_logged-in navigation_invisible "> 
           <div className="navigation__container">
             <NavLink to="" className="navigation__link navigation__link_type_home">Главная</NavLink>
             <NavLink to="" className="navigation__link navigation__link_type_movies">Фильмы</NavLink>
