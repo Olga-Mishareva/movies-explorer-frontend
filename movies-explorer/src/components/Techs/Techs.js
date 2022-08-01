34e33a1b7a7a1d80f5b3732c9530902041ce4ea7
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-01 12:50:30 +0200

    feat: add techs for tablet & mobile

--- movies-explorer/src/components/Techs/Techs.js
+++ movies-explorer/src/components/Techs/Techs.js
@@ -3,7 +3,7 @@ import './Techs.css';
 function Techs() {
   return (
     <div className="techs">
-      <h2 className='techs-heading'>Технологии</h2>
+      <h2 className='techs__heading'>Технологии</h2>
       <h3 className='techs__title'>7 технологий</h3>
       <p className='techs__subtitle'>На курсе веб-разработки мы освоили технологии, которые применили в&nbsp;дипломном проекте.</p>
       <ul className='techs__container page__list'>
