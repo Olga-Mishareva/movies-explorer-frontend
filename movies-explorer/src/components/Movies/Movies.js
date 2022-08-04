7862b25974c67762fbe2b74832ad73f82c65f2f8
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-04 20:34:32 +0200

    feat: add preloader

--- movies-explorer/src/components/Movies/Movies.js
+++ movies-explorer/src/components/Movies/Movies.js
@@ -1,7 +1,8 @@
 import './Movies.css';
 import SearchForm from '../SearchForm/SearchForm';
-import Preloader from '../Preloader/Preloader';
+// import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
+import Paganation from '../Paganation/Paganation';
 
 function Movies() {
   return (
@@ -9,6 +10,7 @@ function Movies() {
       <SearchForm />
       {/* <Preloader /> */}
       <MoviesCardList />
+      <Paganation />
     </div>
   );
 }
