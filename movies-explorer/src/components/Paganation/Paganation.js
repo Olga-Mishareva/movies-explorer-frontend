5da5705fdda91fa450581f9b8f8653e750143ee7
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-04 20:34:18 +0200

    feat: add pantagonation

---
+++ movies-explorer/src/components/Paganation/Paganation.js
@@ -0,0 +1,11 @@
+import './Paganation.css';
+
+function Paganation() {
+  return (
+    <div className="paganation"> 
+      <button className='paganation__more-btn' type='button'>Ещё</button>
+    </div>
+  );
+}
+
+export default Paganation;
