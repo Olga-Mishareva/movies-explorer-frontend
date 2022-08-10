e49fdfb8bc8cd0ac043b4430bb49fdc99e7a855b
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-10 15:17:59 +0200

    fix: saved & liked card

--- movies-explorer/src/components/SearchForm/SearchForm.js
+++ movies-explorer/src/components/SearchForm/SearchForm.js
@@ -7,7 +7,7 @@ function SearchForm() {
       <div className='search__container'>
         <form className="search__form" action='#' method='post'> 
           <div className='search__line'>
-            <input className='search__inpup' type='search' name='search' placeholder='Фильм' autoComplete required></input>
+            <input className='search__input' type='search' name='search' placeholder='Фильм' required></input>
             <button className='search__submit-btn' type='submit'></button>
           </div>
           <FilterCheckbox />
