8f1db9dc95d1e0899f2897d4bf673b5dd28fe069
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-12 17:15:07 +0200

    feat: add smooth appearance

--- movies-explorer/src/components/Navigation/Navigation.js
+++ movies-explorer/src/components/Navigation/Navigation.js
@@ -10,30 +10,45 @@ function Navigation({ loggedIn,login }) {
     setMenuIsOpen(!menuIsOpen);
   }
 
+
   return (
     <div className="nav">
-      {/* nav_invisible добавл. состоянием, если залогинен */}
+  
       <nav className={`nav nav_type_logged-out nav_${loggedIn ? 'invisible' : ''}`}> 
-        <NavLink to="/signup" className="nav__link nav__link_type_register">Регистрация</NavLink>
-        <NavLink to="/signin" className="nav__link nav__link_type_login" onClick={login}>Войти</NavLink>
+        <NavLink to="/signup" className="nav__link nav__link_type_register">
+          Регистрация
+        </NavLink>
+        <NavLink to="/signin" className="nav__link nav__link_type_login" onClick={login}>
+          Войти
+        </NavLink>
       </nav>
-      {/* background-image меняется состоянием при нажатии, invisible добавл. если не залог. */}
+
       <button className={`nav__button nav__button_type_${!menuIsOpen ? 'burger' : 'close'} 
         nav__button_${loggedIn ? '' : 'invisible'}`} 
-        type="button" onClick={handleMenu}></button> 
-      {/* overlay_invisible добавляется состоянием при нажатии на nav__button, меню скрывается */}
-      <div className={`nav__overlay nav__overlay_${menuIsOpen ? '' : 'invisible'}`}> 
-        {/* nav_invisible добавл. состоянием, если не залогинен */}
+        type="button" onClick={handleMenu}></button>    
+      <div className={`nav__side-menu nav__side-menu_${menuIsOpen ? '' : 'invisible'}`}> 
         <nav className={`nav nav_type_logged-in nav_${loggedIn ? '' : 'invisible'}`}> 
           <div className="nav__container">
-            <NavLink to="/" className='nav__link nav__link_type_home' onClick={handleMenu}>Главная</NavLink>
-            <NavLink to="/movies" className="nav__link nav__link_type_movies" onClick={handleMenu}>Фильмы</NavLink>
-            <NavLink to="/saved-movies" className="nav__link nav__link_type_saved-movies" onClick={handleMenu}>Сохранённые фильмы</NavLink>
+            <NavLink to="/" className='nav__link nav__link_type_home' 
+              onClick={handleMenu}>
+                Главная
+            </NavLink>
+            <NavLink to="/movies" className="nav__link nav__link_type_movies"
+              onClick={handleMenu}>
+                Фильмы
+            </NavLink>
+            <NavLink to="/saved-movies" className="nav__link nav__link_type_saved-movies" 
+              onClick={handleMenu}>
+                Сохранённые фильмы
+            </NavLink>
           </div>
-          <NavLink to="/profile" className="nav__link nav__link_type_profile" onClick={handleMenu}>Аккаунт
+          <NavLink to="/profile" className="nav__link nav__link_type_profile" 
+            onClick={handleMenu}>
+              Аккаунт
             <img className="nav__user-icon" src={userIcon} alt="Иконка пользователя"></img>
           </NavLink>
         </nav>
+
       </div>
     </div>
   );
