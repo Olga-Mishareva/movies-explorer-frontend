8328bb1a31d8b2825f3b5059454d4d028a714baa
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-14 12:52:04 +0200

    feat: add lang change

--- movies-explorer/src/components/App/App.js
+++ movies-explorer/src/components/App/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
@@ -10,11 +11,15 @@ import Login from '../Login/Login';
 import NoMatch from '../NoMatch/NoMatch';
 // import InfoPopup from '../InfoPopup/InfoPopup';
 import useLogin from '../../utils/useLogin';
+import { lang } from '../../utils/constants/constants';
 import './App.css';
 
 function App() {
   const { loggedIn, login, logout } = useLogin();
 
+  useEffect(() => {
+    document.documentElement.lang = lang;
+  }, []);
 
   return (
     <div className='page'>
