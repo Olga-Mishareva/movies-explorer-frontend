b95fef75ed6ed08d260233e855b13700f09a466c
Olga-Mishareva <mishareva.olga@yandex.ru>
2022-08-22 10:31:40 +0200

    fix: greeting in profile

--- movies-explorer/src/components/Profile/Profile.js
+++ movies-explorer/src/components/Profile/Profile.js
@@ -75,7 +75,7 @@ function Profile({ onLogout, onUpdate }) {
       <form className='profile__form' 
         name='profile' id='profile'
         onChange={checkErrors} onSubmit={handleSubmit} >
-        <h2 className='profile__greeting'>Привет, Оля!</h2>
+        <h2 className='profile__greeting'>{`Привет, ${currentUser.username}!`}</h2>
         <label className='form-input__label form-input__label_type_profile'>Имя
           <input className={`form-input__input form-input__input_type_profile
             form-input__input_type_${isInputValid ? '' : 'error'}`} 
