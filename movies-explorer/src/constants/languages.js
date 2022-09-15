1e2dc803241dc8098f005b5ef61310c5e35b6c1c
Olga-Mishareva <mishareva.olga@gmail.com>
2022-09-15 13:52:12 +0200

    feat: add languages objects

---
+++ movies-explorer/src/constants/languages.js
@@ -0,0 +1,133 @@
+export const EN = {
+  login: 'Log in',
+  loginGreeting: 'Nice to see you!',
+  logging: 'Logging in...',
+  logout: 'Log out',
+  register: 'Register',
+  registerGreeting: 'You are welcome!',
+  registration: 'Registration...',
+  registered: 'Not registered?',
+  placeholderName: 'Enter your name',
+  placeholderEmail: 'Enter your email',
+  password: 'Password',
+  placeholderPassword: 'Enter your password',
+
+  profile: 'Profile',
+  profileGreeting: 'Hello',
+  profileName: 'Name',
+  profileNewName: 'New name',
+  profileNewEmail: 'New email',
+  saveBtn: 'Save',
+  saving: 'Saving...',
+  canselBtn: 'Cancel',
+
+  movies: 'Movies',
+  savedMovies: 'Saved movies',
+  placeholderMovie: 'Movie',
+  shortMovies: 'Short movies',
+
+  heading: 'Web development student project.',
+  knowMore: 'Scroll below to learn more about this project and its creator.',
+  knowMoreBtn: 'Find out more',
+
+  aboutProject: 'About Project',
+  stepsHeading: 'The diploma project included 5 stages',
+  stepsText: 'Making a plan, working on the backend, layout, adding functionality and final refinements.',
+  timeHeading: 'The diploma took 5 weeks to complete',
+  timeText: 'Each stage had soft and hard deadlines that had to be met in order to successfully defend themselves.',
+  backendTime: '1 week',
+  frontendTime: '4 weeks',
+
+  technologies: 'Technologies',
+  techsHeading: '7 technologies',
+  techsText: 'On the course of web development, we mastered the technologies that we used in the thesis project.',
+
+  student: 'Student',
+  studentName: 'Olga',
+  studentAbout: 'Web developer, 38',
+  studentext: 'I live in Berlin. I have worked with clients for many years and have organized more than 40 events with up to 100 participants. I am passionate about photography and love to travel.',
+  
+  portfolio: 'Portfolio',
+  staticwebsite: 'Static website',
+  responsiveWebsite: 'Responsive website',
+  singlePageApp: 'Single-page application',
+
+  credentials: 'Y.Praktikum x BeatFilm training project.',
+
+  isSignup: 'Registration is successfully!',
+  notFoundErr: 'Data not found',
+  badRequestErr: 'Invalid data',
+  conflictErr: 'This email already exists',
+  forbiddenErr: 'No rights for this action',
+  authErr: 'Invalid email or password',
+  serverErr: 'Server error',
+  pathNotFound: 'Path not found',
+  unauthorized: 'Authorisation is required'
+}
+
+export const RU = {
+  login: 'Вход',
+  loginGreeting: 'Рады видеть!',
+  logging: 'Вход...',
+  logout: 'Выйти из аккаунта',
+  register: 'Зарегистрироваться',
+  registerGreeting: 'Добро пожаловать!',
+  registration: 'Регистрация...',
+  registered: 'Еще на зарегистрированы?',
+  placeholderName: 'Как вас называть?',
+  placeholderEmail: 'Введите ваш email',
+  password: 'Пароль',
+  placeholderPassword: 'Введите ваш пароль',
+
+  profile: 'Аккаунт',
+  profileGreeting: 'Привет,',
+  profileName: 'Имя',
+  profileNewName: 'Новое имя',
+  profileNewEmail: 'Новый email',
+  saveBtn: 'Сохранить',
+  saving: 'Сохранение...',
+  canselBtn: 'Отмена',
+
+  movies: 'Фильмы',
+  savedMovies: 'Сохраненные фильмы',
+  placeholderMovie: 'Фильм',
+  shortMovies: 'Короткометражки',
+
+  heading: 'Учебный проект студента факультета Веб‑разработки.',
+  knowMore: 'Листайте ниже, чтобы узнать больше про этот проект и его создателя.',
+  knowMoreBtn: 'Узнать больше',
+
+  aboutProject: 'О проекте',
+  stepsHeading: 'Дипломный проект включал 5 этапов',
+  stepsText: 'Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.',
+  timeHeading: 'На выполнение диплома ушло 5 недель',
+  timeText: 'У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.',
+  backendTime: '1 неделя',
+  frontendTime: '4 недели',
+
+  technologies: 'Технологии',
+  techsHeading: '7 технологий',
+  techsText: 'На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.',
+
+  student: 'Студентка',
+  studentName: 'Ольга',
+  studentAbout: 'Веб-разработчик, 38 лет',
+  studentext: 'Я сейчас живу в Берлине. Много лет работала с клиентами, а также организовала более 40 мероприятий до 100 участников. Увлекаюсь фотографией и люблю путешествовать.',
+
+  portfolio: 'Портфолио',
+  staticwebsite: 'Статичный сайт',
+  responsiveWebsite: 'Адаптивный сайт',
+  singlePageApp: 'Одностраничное приложение',
+
+  credentials: 'Учебный проект Y.Praktikum х BeatFilm.',
+
+  isSignup: 'Вы успешно зарегистрировались!',
+  notFoundErr: 'Data not found',
+  badRequestErr: 'Данные не найдены',
+  conflictErr: 'Такой email уже существует',
+  forbiddenErr: 'Нет прав для данного действия',
+  authErr: 'Некорректный email или пароль',
+  serverErr: 'Произошла ошибка на сервере',
+  pathNotFound: 'Путь не найден',
+  unauthorized: 'Необходима авторизация'
+}
